bdb38804069302bd24d2619a63d719fcade47f35
LAHAN Tresor <haileystone0856@gmail.com>
2023-11-16 23:08:35 +0100

    Adding JS code

--- main2.js
+++ main2.js
@@ -1,12 +1,3 @@
-let cards = document.querySelectorAll('.card');
-
-cards.forEach((card) => {
-    card.addEventListener("click", () => {
-        card.classList.toggle("is-flipped");
-    });
-});
-
-
 class cardElement {
     constructor(name) {
         const newCard = document.createElement("div")
@@ -23,7 +14,11 @@ class cardElement {
     }
 
     add() {
-        document.querySelector(".game").prepend(this.card)
+        document.querySelector(".game").append(this.card)
+    }
+
+    isOpen() {
+        return this.card.firstElementChild.classList.contains('is-flipped')
     }
 }
 
@@ -37,4 +32,33 @@ class cardPair {
         this.firstElement.add()
         this.secondElement.add()
     }
+
+    isValidate() {
+        console.log(this.firstElement.isOpen() && this.secondElement.isOpen())
+        return(this.firstElement.isOpen() && this.secondElement.isOpen())
+    }
+
+    
 }
+
+let pair1 = new cardPair("1")
+let pair2 = new cardPair("2")
+let pair3 = new cardPair("3")
+
+allPairs = [pair1, pair2, pair3]
+
+allPairs.forEach((pair) => {
+    pair.addPair()
+})
+
+let cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("is-flipped");
+    });
+});
+
+document.querySelector('body').addEventListener('click', () => {
+    pair1.isValidate()
+})
